e8cd70be8de085288bb2f6aa6538b839e8fcfe98
Valentino Coronel <coronel.valentino@hotmail.com>
2022-04-12 09:24:36 -0300

    beta10

--- js/app.js
+++ js/app.js
@@ -12,34 +12,12 @@ addEventListener("DOMContentLoaded", () => {
 });
 
 
-/* SCROLL CON VP */
-
-// función para obtener el ancho de la barra de scroll
-const getScrollBarWidth = () =>
-  window.innerWidth - document.documentElement.getBoundingClientRect().width;
-
-// funcion para asignar ese valor a una variable css
-const cssScrollBarWidth = () =>
-  document.documentElement.style.setProperty(
-    "--scrollbar",
-    `${getScrollBarWidth()}px`
-  );
-
-// asignar la variable css al cargar la página
-addEventListener("load", cssScrollBarWidth);
-
-// reasignar la variable css al redimensionar la ventana
-addEventListener("resize", cssScrollBarWidth);
-
-
 /* SOMBRA NAV */
 $(window).scroll(function () {
     if ($(window).scrollTop() + $(window).height() <= $(document).height()) {
       //Al llegar al pie de la pagina se ejecuta
       $(".menu").css({ "box-shadow": "0px 2px 10px" });
-    } else {
-      $(".menu").css({ "box-shadow": "0px 2px 10px" });
-    }
+    } 
   });
   
   $(window).scroll(function () {
